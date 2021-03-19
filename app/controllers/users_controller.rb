6345f86d56cc1cc5968dcadb9d2c7c39c5514939
KAZUKI27854 <02kazuki16@gmail.com>
2021-03-19 07:36:16 +0000

    create-home-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,23 @@
 class UsersController < ApplicationController
-  def new
-  end
-  
   def create
+    @user = User.new
+    redirect_to user_path(current_user.id)
   end
-  
+
   def show
   end
-  
+
   def index
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
-  
+
+
 end
