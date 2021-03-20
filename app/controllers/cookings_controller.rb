a54287fb1d56c491c52019cfb23a1271f4115634
KAZUKI27854 <02kazuki16@gmail.com>
2021-03-20 03:44:35 +0000

    create show,index,edit

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -1,33 +1,43 @@
 class CookingsController < ApplicationController
   def new
-    
+    @cooking = Cooking.new
   end
-  
+
   def create
     @cooking = Cooking.new(cooking_params)
+    @cooking.user_id = current_user.id
     @cooking.save
-    redirect_to cooking_path
+    redirect_to cooking_path(@cooking.id)
   end
-  
+
   def show
+    @cooking = Cooking.find(params[:id])
   end
-  
+
   def index
+    @cookings = Cooking.all
   end
-  
+
   def edit
+    @cooking = Cooking.find(params[:id])
   end
-  
+
   def update
+    @cooking = Cooking.find(params[:id])
+    @cooking.update(cooking_params)
+    redirect_to cooking_path(@cooking.id)
   end
-  
+
   def destroy
+    @cooking = Cooking.find(params[:id])
+    @cooking.destroy
+    redirect_to cookings_path
   end
-  
+
   private
-  
+
   def cooking_params
     params.require(:cooking).permit(:title, :ingredients, :process, :cooking_image)
   end
-  
+
 end
--- app/models/cooking.rb
+++ app/models/cooking.rb
@@ -1,5 +1,6 @@
 class Cooking < ApplicationRecord
-  attachment :cooking_image
-  
+
   belongs_to :user
+  attachment :cooking_image
+
 end
